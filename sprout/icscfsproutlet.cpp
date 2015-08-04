64107fde63c03d72c9afd8013277f1ce05373be1
Tom Rees <Thomas.Rees@metaswitch.com>
2015-08-04 16:56:08 +0100

    Suppress Tel URI conversion in I-CSCF if number not global

--- sprout/icscfsproutlet.cpp
+++ sprout/icscfsproutlet.cpp
@@ -511,14 +511,16 @@ void ICSCFSproutletTsx::on_rx_initial_request(pjsip_msg* req)
     _originating = false;
     pjsip_uri* uri = PJUtils::term_served_user(req);
 
-    // If the Req URI is a SIP URI with the user=phone parameter set, we should
-    // replace it with a tel URI, as per TS24.229 5.3.2.1.
+    // If the Req URI is a SIP URI with the user=phone parameter set, is not a
+    // GRUU and the user part starts with '+' (i.e. is a global phone number),
+    // we should replace it with a tel URI, as per TS24.229 5.3.2.1.
     if (PJSIP_URI_SCHEME_IS_SIP(uri))
     {
       pjsip_sip_uri* sip_uri = (pjsip_sip_uri*)uri;
 
       if ((!pj_strcmp(&sip_uri->user_param, &STR_USER_PHONE)) &&
           (PJUtils::is_user_numeric(sip_uri->user)) &&
+          (PJUtils::is_user_global(sip_uri->user)) &&
           (!PJUtils::is_uri_gruu(uri)))
       {
         LOG_DEBUG("Change request URI from SIP URI to tel URI");
@@ -568,6 +570,7 @@ void ICSCFSproutletTsx::on_rx_initial_request(pjsip_msg* req)
     if ((!_icscf->should_require_user_phone()) &&
         (PJSIP_URI_SCHEME_IS_SIP(uri)) &&
         (PJUtils::is_user_numeric(((pjsip_sip_uri*)uri)->user)) &&
+        (PJUtils::is_user_global(((pjsip_sip_uri*)uri)->user)) &&
         (!PJUtils::is_uri_gruu(uri)))
     {
       LOG_DEBUG("enforce_user_phone set to false, try using a tel URI");
