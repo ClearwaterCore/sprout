b6bf580f8a7530fde8b026dff63e60066bf9d60a
Colin Haywood <colin.haywood@metaswitch.com>
2017-10-10 16:21:29 +1300

    Raise SAS log when Sprout decides not to do an ENUM lookup

--- src/pjutils.cpp
+++ src/pjutils.cpp
@@ -2311,6 +2311,12 @@ void PJUtils::translate_request_uri(pjsip_msg* req,
       }
     }
   }
+  else if (uri_class == LOCAL_PHONE_NUMBER)
+  {
+    TRC_DEBUG("Not doing ENUM lookup as URI was classified as local DN");
+    SAS::Event event(trail, SASEvent::NO_ENUM_LOOKUP_LOCAL_DN, 0);
+    SAS::report_event(event);
+  }
 }
 
 void PJUtils::update_request_uri_np_data(pjsip_msg* req,
@@ -2361,6 +2367,12 @@ void PJUtils::update_request_uri_np_data(pjsip_msg* req,
         }
       }
     }
+  }
+  else if (uri_class == LOCAL_PHONE_NUMBER)
+  {
+    TRC_DEBUG("Not doing ENUM lookup as URI was classified as local DN");
+    SAS::Event event(trail, SASEvent::NO_ENUM_LOOKUP_LOCAL_DN, 0);
+    SAS::report_event(event);
   }
   else
   {
